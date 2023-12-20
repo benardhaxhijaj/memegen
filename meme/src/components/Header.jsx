e52d00d78d71a8ebcb16fd24dee981f42691708f
Benard Haxhijaj <benardhaxhijaj@gmail.com>
2023-12-20 15:40:56 +0100

    Setting up The Header

---
+++ meme/src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="header-div">
+      <img src="./src/img/troll.png" />
+      <h2>Meme Generator</h2>
+      <span>React Course - Project 3</span>
+    </header>
+  );
+};
+
+export default Header;
