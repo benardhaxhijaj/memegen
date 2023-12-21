c77e098a8b0ebf4f64e79dace571088396893b44
Benard Haxhijaj <benardhaxhijaj@gmail.com>
2023-12-21 19:06:15 +0100

    Fixing Space

--- meme/src/components/Form.jsx
+++ meme/src/components/Form.jsx
@@ -2,10 +2,16 @@ const Form = () => {
   return (
     <main>
       <form className="form-div">
-        <input type="text" placeholder="Top Text" className="input-div" />
-        <input type="text" placeholder="Bottom Text" className="input-div" />
-        <button className="btn-div">Get a new meme image 🖼</button>
+        <div className="form-container">
+          <label>Top Text</label>
+          <input type="text" placeholder="Top Text" className="input-div" />
+        </div>
+        <div className="form-container">
+          <label>Bottom Text</label>
+          <input type="text" placeholder="Bottom Text" className="input-div" />
+        </div>
       </form>
+      <button className="btn-div">Get a new meme image 🖼</button>
     </main>
   );
 };
