416f359f1e9562fc54cededcee9c5521341cdbfb
Benard Haxhijaj <benardhaxhijaj@gmail.com>
2023-12-21 19:15:36 +0100

    Fixing labels and fonts

--- meme/src/components/Form.jsx
+++ meme/src/components/Form.jsx
@@ -4,11 +4,15 @@ const Form = () => {
       <form className="form-div">
         <div className="form-container">
           <label>Top Text</label>
-          <input type="text" placeholder="Top Text" className="input-div" />
+          <input type="text" placeholder="Shut up" className="input-div" />
         </div>
         <div className="form-container">
           <label>Bottom Text</label>
-          <input type="text" placeholder="Bottom Text" className="input-div" />
+          <input
+            type="text"
+            placeholder="And take my money"
+            className="input-div"
+          />
         </div>
       </form>
       <button className="btn-div">Get a new meme image 🖼</button>
