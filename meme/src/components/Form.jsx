5dfd7528d54a09eecccc7bd68b238879194a7620
Benard Haxhijaj <benardhaxhijaj@gmail.com>
2023-12-30 21:50:14 +0100

    setting up text

--- meme/src/components/Form.jsx
+++ meme/src/components/Form.jsx
@@ -22,26 +22,50 @@ const Form = () => {
     }));
   };
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
+  const { topText, bottomText } = meme;
+
   return (
     <main>
       <form className="form-div">
         <div className="form-container">
           <label>Top Text</label>
-          <input type="text" placeholder="Shut up" className="input-div" />
+          <input
+            onChange={handleChange}
+            type="text"
+            placeholder="Shut up"
+            className="input-div"
+            name="topText"
+            value={topText}
+          />
         </div>
         <div className="form-container">
           <label>Bottom Text</label>
           <input
+            onChange={handleChange}
             type="text"
             placeholder="And take my money"
             className="input-div"
+            name="bottomText"
+            value={bottomText}
           />
         </div>
       </form>
       <button className="btn-div" onClick={getMemeImg}>
         Get a new meme image 🖼
       </button>
-      <img src={meme.randomImage} className="meme-img" />
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 };
